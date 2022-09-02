6c20cf694498ee2385685e1c00ca0cc6a16fd6f2
He Sun <wushanyun64@gmail.com>
2022-09-02 00:44:43 -0500

    Update doc

--- src/predict.py
+++ src/predict.py
@@ -8,19 +8,20 @@ from src.features_gen import table_clean
 
 
 class predict:
-    """_summary_"""
-
     def __init__(self, model_name, feature_type, structure_list):
-        """_summary_
+        """
+        Utility obj to construct the pipeline to make predictions with pretrained models.
 
         Parameters
         ---------------------------
-        model_name (_type_): _description_
+        model_name: str
+            name of the trained model that can be used, model can be find in ./models.
         feature_type: str
             A string that indicates which feature set to use, either "struc" or "struc+ele".
             The type of feature should match the type of trained model.
         structure_list: list
-            _description_
+            A list of structures for prediction. The structures stored need to be compatible with
+            pymatgen.Structure, otherwise an error will raise.
         """
         self.feat_type = feature_type
         # import trained models
@@ -39,7 +40,10 @@ class predict:
         self.prep()
 
     def model_predict(self):
-        """_summary_"""
+        """
+        The function to make the prediction and return the original labels (y) and
+        predicted labels (y_pre).
+        """
         features = self.feature_generation()
         if self.feat_type == "struc":
             x = features.loc[:, "fbl_average":"DI"]
@@ -55,6 +59,9 @@ class predict:
         return y, y_pre
 
     def feature_generation(self):
+        """
+        Helper function to call feature generation functions.
+        """
         features = concat_features_and_nmr(self.structure_tensors)
         features.reset_index(drop=True, inplace=True)
         features = table_clean(features)
@@ -71,25 +78,17 @@ class predict:
         self._filter_by_chemical_env()
 
     def _add_oxy(self):
-        """_summary_
-
-        Returns:
-            _type_: _description_
-        """
         self.structure_tensors = add_oxi_state_by_guess(self.structure_tensors)
 
     def _filter_by_coordination(self):
-        """_summary_"""
         self.structure_tensors = get_n_coord_tensors(
             self.structure_tensors, coord=[4, 5, 6]
         )
 
     def _append_coordination(self):
-        """_summary_"""
         self.structure_tensors = append_coord_num(self.structure_tensors)
 
     def _append_chemical_env(self):
-        """_summary_"""
         self.structure_tensors = append_ce(self.structure_tensors)
 
     def _filter_by_chemical_env(self):
